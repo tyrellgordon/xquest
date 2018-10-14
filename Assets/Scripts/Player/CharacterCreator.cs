368ba3ac7908aa49391f0bb3184ab6092f98df20
doctorpillcosby <gordontyrell1993@gmail.com>
2018-10-14 00:57:51 -0400

    Finished name select functionality, finished main game loop

---
+++ Assets/Scripts/Player/CharacterCreator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class CharacterCreator : MonoBehaviour {
+
+    public InputField nameField;
+
+    public string charName;
+
+
+    public void OnSubmit(){
+        charName = nameField.text;
+
+        Player.playerName = charName;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+}
