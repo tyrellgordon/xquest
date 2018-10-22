e22b7a84376873dd0ce2777984a926f0f4868f60
doctorpillcosby <gordontyrell1993@gmail.com>
2018-10-22 14:01:32 -0400

    added snake tracking

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+
+    public float speed;
+    private Transform target;
+    public float followDistance;
+
+
+	// Use this for initialization
+	void Start () {
+
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Vector2.Distance(transform.position, target.position) < followDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+
+	}
+}
